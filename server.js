8c0a43eb51ea10492680398c40e7f0d4c8d03790
Anurag Gupta <aguptaking@gmail.com>
2021-04-06 15:44:25 +0530

    Production Env

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(express.json());
 app.use(gradesRouter);
 app.use(quizRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const server = require("http").createServer(app);
 
 server.listen(process.env.PORT, () => {
