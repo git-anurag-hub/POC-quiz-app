839055697f8acf5f4bac75f2f06f8adf2fb942fa
Anurag Gupta <aguptaking@gmail.com>
2021-04-06 16:01:52 +0530

    Solved Server Path error bug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const gradesRouter = require("./src/routes/grades");
 const quizRouter = require("./src/routes/quiz");
+const path = require("path");
 
 require("./src/db/mongoose");
 
